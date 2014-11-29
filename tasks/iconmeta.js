f9ccb80717b672cc0bd9c5bb496803390da82ac0
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-11-29 15:42:12 +0800

    Parse source CSS files and save as meta files

--- tasks/iconmeta.js
+++ tasks/iconmeta.js
@@ -15,17 +15,61 @@ module.exports = function (grunt) {
     var taskSources = source === 'all' ? sourceList : [source];
     _.each(taskSources, function (source) {
       var sourceManifest = grunt.file.readJSON(path.join(sourcesPath, source + '.json'));
-      var cssPath = path.join(componentsPath, sourceManifest.main);
-      var file = grunt.file.read(cssPath);
-
+      var sourceBower = grunt.file.readJSON(path.join(componentsPath, sourceManifest.bower.packageName, 'bower.json'));
+      var file = grunt.file.read(path.join(componentsPath, sourceManifest.bower.packageName, sourceManifest.bower.main));
       var re = new RegExp(sourceManifest.selectorRegex, 'gi');
-      var matches = re.exec(file);
+
       var allNames = [];
-      while (matches) {
-        allNames.push(matches[1]);
-        matches = re.exec(file);
+      var match;
+      while (match = re.exec(file)) {
+        allNames.push(match[1]);
       }
-      allNames = allNames.sort();
+      allNames = _.uniq(allNames.sort(), true);
+      
+      var metaFilePath = path.join(metaPath, source + '-meta.json');
+      if (grunt.file.exists(metaFilePath)) {
+        var metaFile = grunt.file.readJSON(metaFilePath);
+        var existingIconLookup = {};
+        _.each(metaFile.icons, function (item) {
+          existingIconLookup[item.name] = item;
+        });
+        var existingIconsNameList = _.pluck(metaFile.icons, 'name');
+        var combinedIconList = _.union(allNames, existingIconsNameList).sort();
+        var iconsMetaList = [];
+        _.each(combinedIconList, function (name) {
+          if (existingIconLookup[name]) {
+            iconsMetaList.push(existingIconLookup[name]);
+          } else {
+            iconsMetaList.push( {
+              name: name,
+              categories: [],
+              tags: []
+            });
+          }
+        });
+      } else {
+        var iconsMetaList = _.map(allNames, function (name) {
+          return {
+            name: name,
+            categories: [],
+            tags: []
+          };
+        });
+      }
+
+      var iconsData = {
+        name: sourceManifest.name,
+        slug: sourceManifest.slug,
+        homepage: sourceManifest.homepage,
+        usage: sourceManifest.usage,
+        version: sourceBower.version,
+        icons: iconsMetaList
+      };
+      
+      grunt.file.write(
+        metaFilePath,
+        JSON.stringify(iconsData, null, 2)
+      );
     });
   });
 };
